c72d65140e9b64e2fa312188ad4c13fcba1935b2
leo <leo@134.155.248.149>
2014-11-23 15:58:24 +0100

    update() implementiert

--- uebung3/src/uebung3/BinaerHashTree.java
+++ uebung3/src/uebung3/BinaerHashTree.java
@@ -3,200 +3,200 @@ package uebung3;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
-public class BinaerHashTree<T, U,R> implements AssociativeArray<T, U,R> {
-	
+public class BinaerHashTree<T, U, R> implements AssociativeArray<T, U, R> {
+
 	public TreeNode root;
-	int counter=0;
-	
+	int counter = 0;
+
 	class TreeNode {
 
-	    U value;
-	    T key;
-	    
-	    
-	    TreeNode left = null;
-	    TreeNode right = null;
-	    TreeNode parent = null;
+		U value;
+		T key;
 
-	   //Objektwerte werden in einen Knoten reingesetzt
-	    public TreeNode(U value, T key) {
-	        this.value = value;
-	        this.key = key;
-	    }
+		TreeNode left = null;
+		TreeNode right = null;
+		TreeNode parent = null;
 
-	    // left und right wird erzeugt
-	    public TreeNode(U value, T key, TreeNode left, TreeNode right) {
-	        this(value,key);
-	        this.left = left;
-	        this.right = right;
-	    }
+		// Objektwerte werden in einen Knoten reingesetzt
+		public TreeNode(U value, T key) {
+			this.value = value;
+			this.key = key;
+		}
 
-	    
-	    public void setLeft(TreeNode n) {
-	        left = n;
-	        n.parent = this;
-	        
-	    }
+		// left und right wird erzeugt
+		public TreeNode(U value, T key, TreeNode left, TreeNode right) {
+			this(value, key);
+			this.left = left;
+			this.right = right;
+		}
 
-	    public void setRight(TreeNode n) {
-	        right = n;
-	        n.parent = this;
-	    }
-	
-	    public void print() {
-	        System.out.print(value + " " + key.hashCode() + '\n');
-	    }
+		public void setLeft(TreeNode n) {
+			left = n;
+			n.parent = this;
 
-	    public String toString() {
-	        return value + " "  + key;
-	        
-	    }
+		}
 
-	    public void printReversePostorder() {
-	        printReversePostorder(this);
-	    }
+		public void setRight(TreeNode n) {
+			right = n;
+			n.parent = this;
+		}
 
-	    public void printReversePostorder(TreeNode n) {
-	        // Ist der Baum nicht leer?
-	        if (n != null) {
-	            // Dann kann weiter gemacht werden.
-	            printReversePostorder(n.right);
-	            printReversePostorder(n.left);
-	            n.print();
-	        }
-	    }
+		public void print() {
+			System.out.print(value + " " + key.hashCode() + '\n');
+		}
+
+		// ok
+		public String toString() {
+			return value + " " + key;
+
+		}
+
+		public void printReversePostorder() {
+			printReversePostorder(this);
+		}
+
+		public void printReversePostorder(TreeNode n) {
+			// Ist der Baum nicht leer?
+			if (n != null) {
+				// Dann kann weiter gemacht werden.
+				printReversePostorder(n.right);
+				printReversePostorder(n.left);
+				n.print();
+			}
+		}
 	}
-	public TreeNode getNode(T  key){
-		
+
+	// ok
+	public TreeNode getNode(T key) {
+
 		return getNodeRek(key, root);
 	}
-	
-	public TreeNode getNodeRek(T key, TreeNode node){
-		if (node != null){
-			if(node.key.hashCode() == key.hashCode()) {
+
+	// ok
+	public TreeNode getNodeRek(T key, TreeNode node) {
+		if (node != null) {
+			if (node.key.hashCode() == key.hashCode()) {
 				return node;
-			}else if(node.key.hashCode() > key.hashCode()) {
+			} else if (node.key.hashCode() > key.hashCode()) {
 				return getNodeRek(key, node.left);
-			}else {
+			} else {
 				return getNodeRek(key, node.right);
 			}
 		}
 		return null;
 	}
-	
-	//ok
+
+	// ok
 	@Override
 	public void clear() {
 		root = null;
 	}
-    //ok
+
+	// ok
 	@Override
 	public boolean containsKey(T key) {
 		return contains(root, key.hashCode());
 	}
-	//ok
+
+	// ok
 	@Override
 	public boolean containsValue(U value) {
-			return contains(root, value.hashCode());
+		return contains(root, value.hashCode());
 	}
-	//ok
-	public boolean contains(TreeNode node, int hash){
-		if(node != null){
-			if(node.key.hashCode() == hash || node.value.hashCode() == hash){
+
+	// ok
+	public boolean contains(TreeNode node, int hash) {
+		if (node != null) {
+			if (node.key.hashCode() == hash || node.value.hashCode() == hash) {
 				return true;
-			} else if (node.key.hashCode() > hash){
+			} else if (node.key.hashCode() > hash) {
 				return contains(node.left, hash);
-			} else { 
+			} else {
 				return (contains(node.right, hash));
 			}
 		}
 		return false;
 	}
-	//ok
+
+	// ok
 	@Override
-	public U get(T key){
-        TreeNode node = root; 
-        
-        if(containsKey(key)){
-        while (key.hashCode() != node.key.hashCode()) {
-          
-            if (key.hashCode() < node.key.hashCode()) {
-                node = node.left;
-            } else {
-            	node = node.right;
-            }
-        }
-        return node.value;
-        }
-        return null;
+	public U get(T key) {
+		if (containsKey(key)) {
+			return getNode(key).value;
+		}
+		return null;
 	}
-    //ok
+
+	// ok
 	@Override
 	public boolean isEmpty() {
 		return this.root == null;
 	}
-    //ok
+
+	// ok
 	@Override
 	public void put(T key, U value) {
 		put(new TreeNode(value, key));
-		
+
 	}
-	//ok
-	public void put(TreeNode knoten){
-		
-        TreeNode parent = null;
-        TreeNode node = root; 
-        
-        while (node != null) {
-            parent = node;
-            
-            if (knoten.key.hashCode() == node.key.hashCode()) { 	
-                return;
-            } else if (knoten.key.hashCode() < node.key.hashCode()) {
-                node = node.left;
-            } else {
-            	node = node.right;
-            }
-        }
-        
-        if (parent == null) {
-            root = knoten;
-        } else if (knoten.key.hashCode() < parent.key.hashCode()) {
-            parent.setLeft(knoten);
-        } else {            
-            parent.setRight(knoten);
-        }
-     }
+
+	// ok
+	public void put(TreeNode knoten) {
+
+		TreeNode parent = null;
+		TreeNode node = root;
+
+		while (node != null) {
+			parent = node;
+
+			if (knoten.key.hashCode() == node.key.hashCode()) {
+				return;
+			} else if (knoten.key.hashCode() < node.key.hashCode()) {
+				node = node.left;
+			} else {
+				node = node.right;
+			}
+		}
+
+		if (parent == null) {
+			root = knoten;
+		} else if (knoten.key.hashCode() < parent.key.hashCode()) {
+			parent.setLeft(knoten);
+		} else {
+			parent.setRight(knoten);
+		}
+	}
 
 	@Override
-	public void putAll(BinaerHashTree<? extends T,? extends U,? extends R> knoten) {
+	public void putAll(
+			BinaerHashTree<? extends T, ? extends U, ? extends R> knoten) {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void remove(T key) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
 
-	
-	//ok
+	// ok
 	@Override
-	public int size(){
-		if (root == null){
+	public int size() {
+		if (root == null) {
 			return counter;
-		}else {
+		} else {
 			counter++;
 			return size(root);
-		}	
+		}
 	}
-	//ok
-	public int size(TreeNode a){
-		//int size = 0;
-		if (a.left != null){
+
+	// ok
+	public int size(TreeNode a) {
+		// int size = 0;
+		if (a.left != null) {
 			counter++;
 			size(a.left);
 		}
-		if(a.right != null){
+		if (a.right != null) {
 			counter++;
 			size(a.right);
 		}
@@ -205,9 +205,9 @@ public class BinaerHashTree<T, U,R> implements AssociativeArray<T, U,R> {
 
 	@Override
 	public void update(T key, U value) {
-		
-		
-		
+		if (containsKey(key)) {
+			getNode(key).value = value;
+		}
 	}
 
 	@Override
@@ -217,16 +217,16 @@ public class BinaerHashTree<T, U,R> implements AssociativeArray<T, U,R> {
 
 	@Override
 	public void extractAll() {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void map(BiFunction<? super T, ? super U, ? super R> function) {
 		// TODO Auto-generated method stub
 	}
-	
-	 public void printReversePostorder() {
-	        root.printReversePostorder();
-	 }
+
+	public void printReversePostorder() {
+		root.printReversePostorder();
+	}
 
 }
--- uebung3/src/uebung3/Test.java
+++ uebung3/src/uebung3/Test.java
@@ -17,11 +17,12 @@ public class Test {
 		
 		//System.out.println(tree.containsValue("acht"));
 		//System.out.println(tree.containsKey(10));
-		//System.out.println(tree.get(5));
-		System.out.println(tree.size());
-		System.out.println(tree.getNode(5));
+		//System.out.println(tree.get(12));
+		//System.out.println(tree.size());
+		//System.out.println(tree.getNode(5));
+		//tree.update(10, "fuenf");
 		
-		//tree.printReversePostorder();
+		tree.printReversePostorder();
 	}
 
 }
